f1e48ae81d6589951f22e79f95d98cdb71ed6c75
Julio César Fernández <jcfmunoz@icloud.com>
2022-02-14 23:02:33 +0100

    New

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public func indiceEn<Element:Equatable>(valor:Element, array:[Element]) -> Int? {
     for (indice, dato) in array.enumerated() {
         if valor == dato {
@@ -5,4 +7,35 @@ public func indiceEn<Element:Equatable>(valor:Element, array:[Element]) -> Int? 
         }
     }
     return nil
+}
+
+enum Gender:String, Codable {
+    case male = "Male"
+    case female = "Female"
+}
+
+struct Empleado:Codable {
+    let id:Int
+    let username:String
+    let first_name:String
+    let last_name:String
+    let gender:Gender
+    let email:String
+    let department:String
+    let address:String
+    var zipcode:String?
+    let avatar:URL
+}
+
+func getEmpleados() -> [Empleado] {
+    guard let url = Bundle.module.url(forResource: "EmpleadosData", withExtension: "json") else {
+        return []
+    }
+    do {
+        let data = try Data(contentsOf: url)
+        return try JSONDecoder().decode([Empleado].self, from: data)
+    } catch {
+        print("Error en la carga \(error)")
+    }
+    return []
 }
